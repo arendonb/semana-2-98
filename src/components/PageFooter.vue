fbb5d99e8ef53c2fe3f78a62fe6c8bd381e2f385
arendonb <arendonb@gmail.com>
2020-12-05 18:51:06 -0500

    Se sube con el member editado. Video 1:25:53

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,54 +1,73 @@
 <template>
-  <footer class="page-footer bg-secondary text-white pt-4">
-    <!-- Footer Text -->
-    <div class="container-fluid text-center text-md-left">
-      <!-- Grid row -->
-      <div class="row">
-        <!-- Grid column -->
-        <div class="col-md-6 mt-md-0 mt-3">
-          <!-- Content -->
-          <h5 class="text-uppercase font-weight-bold">Footer text 1</h5>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita
-            sapiente sint, nulla, nihil repudiandae commodi voluptatibus
-            corrupti animi sequi aliquid magnam debitis, maxime quam recusandae
-            harum esse fugiat. Itaque, culpa?
-          </p>
-        </div>
-        <!-- Grid column -->
+        <footer class="page-footer bg-secondary text-white pt-4">
+        
+        <!-- Footer Text -->
+        <div class="container-fluid text-center text-md-left">
+            
+            <!-- Grid row -->
+            <div class="row">
+                
+                <!-- Grid column -->
+                <div class="col-md-6 mt-md-0 mt-3">
+                    
+                    <!-- Content -->
+                    <h5 class="text-uppercase font-weight-bold">Motivación</h5>
+                    <p>Esta página fue creada con la motivación de aprender a trabajar con Git y GitHub.<br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <a href="https://github.com/arendonb/semana-2-98" target="_blank">Clic AQUÍ para ir al repositorio</a>
+                    <br></p>
+                        
+                    </div>
+                    <!-- Grid column -->
+                    
+                    <hr class="clearfix w-100 d-md-none pb-3">
+                    
+                    <!-- Grid column -->
+                    <div class="col-md-6 mb-md-0 mb-3">
+                        
+                        <!-- Content -->
+                        <h5 class="text-uppercase font-weight-bold">Integrantes</h5>
+                        <p>Alejandro Rendón Blandon - Medellín - arendonb@gmail.com - Product Owner<br>
+                    <br>
 
-        <hr class="clearfix w-100 d-md-none pb-3" />
+                    Santiago Andres Millan Pardo - Bogotá - santiagomillanpardo@gmail.com - Desarrollador BackEnd<br>
+                    <br>
 
-        <!-- Grid column -->
-        <div class="col-md-6 mb-md-0 mb-3">
-          <!-- Content -->
-          <h5 class="text-uppercase font-weight-bold">Footer text 2</h5>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio
-            deserunt fuga perferendis modi earum commodi aperiam temporibus quod
-            nulla nesciunt aliquid debitis ullam omnis quos ipsam, aspernatur id
-            excepturi hic.
-          </p>
-        </div>
-        <!-- Grid column -->
-      </div>
-      <!-- Grid row -->
-    </div>
-    <!-- Footer Text -->
+                    Miguel Enrique Marquez Aldana - Bogotá - miguelmarquez.99@gmail.com - Tester<br>
+                    <br>
 
-    <!-- Copyright -->
-    <div class="footer-copyright text-center py-1 bg-dark">
-      © 2020 Copyright: DAD
-    </div>
-    <!-- Copyright -->
-  </footer>
+                    Diego Luis Soto Mendoza - Villavicencio - diegoluissotomendoza@gmail.com - Tester<br>
+                    <br>
+
+                    Cristhian Daniel Díaz Salazar - Pereira - crdadiaz@utp.edu.co - Scrum Master<br>
+                    <br></p>
+                            
+                        </div>
+                        <!-- Grid column -->
+                        
+                    </div>
+                    <!-- Grid row -->
+                    
+                </div>
+                <!-- Footer Text -->
+                
+                <!-- Copyright -->
+                <div class="footer-copyright text-center py-1 bg-dark">© 2020 Copyright: DAD
+                </div>
+                <!-- Copyright -->
+                
+            </footer>
 </template>
 
 <script>
 export default {
-  name: "PageFooter",
-};
+   name: 'PageFooter', 
+}
 </script>
 
 <style scoped>
+
 </style>
