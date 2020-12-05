93bb7cb5aef4cfbf3f22f1955954f79fad03fa30
arendonb <arendonb@gmail.com>
2020-12-05 17:00:12 -0500

    Se actualiza con el video 1:02:26

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,72 @@
 <template>
-  <div id="app">
+    <div id="app">
+
+  <div class="container-fluid">
+  <div class="row justify-content-center mb-5">
+    <team-card></team-card>
+  </div>
+  </div>
+
+    <page-footer></page-footer>
+    <h1>{{title}}</h1>
   </div>
 </template>
 
 <script>
 
+import PageFooter from './components/PageFooter.vue'
+import TeamCard from './components/TeamCard.vue'
+
+
 export default {
   name: 'App',
+  components:{
+    PageFooter,
+    TeamCard,
+
+  },
+  data(){
+    return{
+      title:'Vivo dentro de VUE',
+      team: [
+        {
+          codigo: 1,
+          nombre: "Primer Integrante",
+          descripcion: '1.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+          rol: "desarrollador Backend",
+          imagen: "https://placeimg.com/192/192/people",
+        },
+        {
+          codigo: 2,
+          nombre: "Segundo Integrante",
+          descripcion: '2.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+          rol: "desarrollador Backend",
+          imagen: "https://placeimg.com/192/192/people",
+        },
+        {
+          codigo: 3,
+          nombre: "Tercer Integrante",
+          descripcion: '3.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+          rol: "desarrollador Backend",
+          imagen: "https://placeimg.com/192/192/people",
+        },
+        {
+          codigo: 4,
+          nombre: "Cuarto Integrante",
+          descripcion: '4.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+          rol: "desarrollador Backend",
+          imagen: "https://placeimg.com/192/192/people",
+        },
+        {
+          codigo: 5,
+          nombre: "Quinto Integrante",
+          descripcion: '5.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+          rol: "desarrollador Backend",
+          imagen: "https://placeimg.com/192/192/people",
+        }
+      ]
+    }
+  }
 }
 </script>
 
