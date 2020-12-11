78ea1651308776e5916b182434e6156ef5426ad2
diegoluissotom <diego-soto@outlook.com>
2020-12-10 19:23:49 -0500

    Se edito el FrontEnd para presentacion visual.

--- src/components/SectionApi.vue
+++ src/components/SectionApi.vue
@@ -1,29 +1,35 @@
 <template>
-  <div>
+  <div id="api" >
     <div class="row mt-md-5 mt-sm-5 mt-xs-5">
       <div
         class="col-lg-6 col-xs-12"
         v-for="(movie, index) of movies"
         :key="index"
       >
-        <div class="d-flex justify-content-center align-items-center">
+        <div class="d-flex justify-content-left align-items-start bg-dark text-white rounded m-3" style="height: 250px;">
           <div class="p-3">
-            <img :src="movie.Poster" alt="movie poster" />
+            <img :src="movie.Poster" alt="movie poster" class="rounded"/>
           </div>
 
-          <div class="p-2">
-            <h2>{{ movie.Title }}</h2>
-            {{ movie.Type }}
-            {{ movie.Year }}
-          </div>
-        </div>
-        <div class="d-flex container-fluid justify-content-end pb-2 mt-n2">
+          <div class="p-2 col">
+            <h2>{{ movie.Title}}</h2>
+            <br>
+            <p><span><b>Tipo: </b> </span>{{ movie.Type }}
+            <br>
+            
+            <span><b>Año: </b> </span>{{ movie.Year }}</p>
+            <br>
+            <br>
+            
+          <div class="d-flex container-fluid justify-content-end pb-2 mt-n2">
           <a
             :href="'https://www.imdb.com/title/' + movie.imdbID"
-            class="btn btn-outline-info"
-            >Info</a
-          >
+            class="btn btn-light " target="_blank"
+            >Info</a>
         </div>
+          </div>
+        </div>
+        
       </div>
     </div>
   </div>
@@ -50,4 +56,8 @@ export default {
 </script>
 
 <style scoped>
+
+img{
+  max-width: 150px;
+};
 </style>
